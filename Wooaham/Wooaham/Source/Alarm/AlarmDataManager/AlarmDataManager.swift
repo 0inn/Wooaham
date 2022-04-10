0298a749aff8752cbbfefd0e4dbd17d955770c4e
0inn <bueaty1220@naver.com>
2022-04-10 22:42:11 +0900

    [+] 알람 서버 통신 파일 추가

---
+++ Wooaham/Wooaham/Source/Alarm/AlarmDataManager/AlarmDataManager.swift
@@ -0,0 +1,8 @@
+//
+//  AlarmDataManager.swift
+//  Wooaham
+//
+//  Created by 김영인 on 2022/04/10.
+//
+
+import Foundation
---
+++ Wooaham/Wooaham/Source/Alarm/AlarmEntity/AlarmEntity.swift
@@ -0,0 +1,8 @@
+//
+//  AlarmEntity.swift
+//  Wooaham
+//
+//  Created by 김영인 on 2022/04/10.
+//
+
+import Foundation
