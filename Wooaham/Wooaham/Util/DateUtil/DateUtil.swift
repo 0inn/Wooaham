268d7152ff429cb99892bfd9f2b3516fddb72e9b
0inn <bueaty1220@naver.com>
2022-05-08 18:22:46 +0900

    [Chore] Util 파일 구조 변경

--- Wooaham/Wooaham/Util/DateUtil/DateUtil.swift
+++ Wooaham/Wooaham/Util/DateUtil/DateUtil.swift
@@ -1,25 +1,11 @@
 //
-//  Functions.swift
+//  DateUtil.swift
 //  Wooaham
 //
 //  Created by 김영인 on 2022/04/14.
 //
 
 import UIKit
-
-// 이미지 설정
-func setIcon(_ imgId: Int) -> UIImage {
-    switch imgId {
-    case 4:
-        return UIImage(named: "house")!
-    case 5:
-        return UIImage(named: "school")!
-    case 6:
-        return UIImage(named: "bus")!
-    default:
-        return UIImage()
-    }
-}
 
 func getToday() -> String {
     let today = NSDate()
