f536116412ef60990373b040ae534f5cafa87d88
김영인 <0inn1220@gmail.com>
2022-05-23 12:16:51 +0900

    [Add] 휴대폰 사용시간 조회 response 추가

---
+++ Wooaham/Wooaham/Network/APIModels/PhoneModels/PhoneTimeResponse.swift
@@ -0,0 +1,18 @@
+//
+//  PhoneTimeResponse.swift
+//  Wooaham
+//
+//  Created by 김영인 on 2022/05/23.
+//
+
+import Foundation
+
+struct PhoneTimeResponse: Codable {
+    let success: Bool
+    let data: [PhoneTimeData]
+    let error: Bool?
+}
+
+struct PhoneTimeData: Codable {
+    let hour, minute: Int?
+}
