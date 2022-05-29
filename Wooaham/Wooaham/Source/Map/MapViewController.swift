5c7100eb7025058ea365e2eb08436a1b691f6744
김영인 <0inn1220@gmail.com>
2022-05-29 21:25:00 +0900

    [Feat] 지도 실시간 위치 받아오기

--- Wooaham/Wooaham/Source/Map/MapViewController.swift
+++ Wooaham/Wooaham/Source/Map/MapViewController.swift
@@ -7,10 +7,68 @@
 
 import UIKit
 
+import CoreLocation
+import NMapsMap
+
 class MapViewController: UIViewController {
+    
+    var locationManager = CLLocationManager()
+    
+    @IBOutlet weak var mapView: NMFMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setMap()
+        setLocation()
     }
     
+    private func setMap() {
+        mapView.positionMode = .direction
+        mapView.zoomLevel = 16.0
+    }
+    
+    private func setCamera() {
+        let cameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(lat: locationManager.location?.coordinate.latitude ?? 0, lng: locationManager.location?.coordinate.longitude ?? 0))
+        cameraUpdate.animation = .easeIn
+        mapView.moveCamera(cameraUpdate)
+        
+        let image = NMFOverlayImage(name: "myLocation")
+        let marker = NMFMarker()
+        marker.iconImage = image
+        marker.width = 80
+        marker.height = 80
+        marker.position = NMGLatLng(lat: locationManager.location?.coordinate.latitude ?? 0, lng: locationManager.location?.coordinate.longitude ?? 0)
+        marker.mapView = mapView
+        setMarkers()
+    }
+    
+    private func setMarkers() {
+        
+    }
+    
+}
+
+extension MapViewController: CLLocationManagerDelegate {
+    private func setLocation() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            print("위치 서비스 On 상태")
+            locationManager.startUpdatingLocation()
+            setCamera()
+        } else {
+            print("위치 서비스 Off 상태")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch(CLLocationManager.authorizationStatus()) {
+        case .authorizedAlways, .authorizedWhenInUse:
+            locationManager.startUpdatingLocation()
+        case .denied, .notDetermined, .restricted:
+            locationManager.stopUpdatingLocation()
+        }
+    }
 }
